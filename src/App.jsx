52e01d6bd290c40742e1fe130c7517cbf2809e2d
Pat <pyy999@gmail.com>
2019-09-14 09:20:36 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './App.css';
+import './Home.jsx'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+const Home = () => {
+	render() {
+		return(
+			<b>ASDF<b>)
+	}
+}
