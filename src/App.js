8f6d6a2d459cff5dad787b58317814ebd4d2c911
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2019-09-15 05:12:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,34 @@ class App extends Component {
       <div>
       
         <Router forceRefresh={!supportsHistory}>
+      
+        <Particles
+                params={{
+                    "particles": {
+                        "line_linked": {
+                                    "color":"#FFFFFF"
+                                    },
+                        "number": {
+                            "value": 150
+                        },
+                        "size": {
+                            "value": 5
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }}
+                style={{
+                        width: '100%',
+                        background: `#000000` 
+                 }}
+                />
      
             
             <div>
